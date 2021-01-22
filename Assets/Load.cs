dba8983e5797fe4f63b2bb0276239810b6894850
Miller1337 <54809020+Miller1337@users.noreply.github.com>
2021-01-22 18:34:21 +0300

    *
    *

---
+++ Assets/Load.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Load : MonoBehaviour
+{
+    public void LoadScene(int Level)
+    {
+        SceneManager.LoadScene(Level);
+    }
+    public void exit()
+    {
+        Application.Quit();
+    }
+}
